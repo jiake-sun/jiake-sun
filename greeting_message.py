a301d723dcb5574b8717800e19af4ff1d02141f7
jiake-sun <sunjiake2014@gmail.com>
2024-11-29 08:24:29 -0500

    Add files via upload

---
+++ greeting_message.py
@@ -0,0 +1,6 @@
+reeting_message = """
+Hey there, you stock market sorcerer! 🪄📈
+
+I hope your crystal ball is working because, frankly, I’m having a hard time telling whether today’s market is going to make me rich or just make me need a drink. Let’s steer this ship toward ‘wealth’ rather than ‘mild disappointment,’ shall we? No pressure, though. Just my entire financial future on the line. 😅💸
+"""
+print(greeting_message)
